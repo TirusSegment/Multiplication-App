a64014dd38f5cd2e856af90b0d666eb6920d09b4
TirusSegment <113556059+TirusSegment@users.noreply.github.com>
2023-04-23 15:28:04 +0530

    added index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+
+const num1 =Math.ceil(Math.random()*10)
+const num2 =Math.ceil(Math.random()*10)
+
+const questionE1 = document.getElementById("question");
+const formE1 = document.getElementById("form")
+const inputE1 = document.getElementById("input")
+
+const scoreE1 = document.getElementById("score")
+
+let score = JSON.parse(localStorage.getItem("score")); // To get the score from the browser local storage.
+
+if(!score){
+    score=0;
+}
+
+scoreE1.innerText=`score: ${score}`
+
+questionE1.innerText=`What is ${num1} multiply by ${num2}?`
+
+const correctAns= num1*num2;
+
+formE1.addEventListener("submit",()=>{
+    const userAns = +inputE1.value;
+    if(userAns == correctAns){
+        score++;
+        updateLocalStorage();
+    }
+    else{
+        score--;
+        updateLocalStorage();
+    }
+});
+
+function updateLocalStorage(){
+    localStorage.setItem("score",JSON.stringify(score)) // It stores the score value in the browser by converting it into a string in a variable named score
+}
+
+
